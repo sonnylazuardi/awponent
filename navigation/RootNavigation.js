d00c8cf8d78d5727bd43cb43af3b5f5794bce03c
Sung Woo Park <spark146@jhu.edu>
2016-11-29 07:21:24 -0800

    need to fix disliking from liked section

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -30,13 +30,6 @@ const store = createStoreWithMiddleware(reducer);
 import registerForPushNotificationsAsync from '../api/registerForPushNotificationsAsync';
 
 export default class RootNavigation extends React.Component {
-  componentDidMount() {
-    this._notificationSubscription = this._registerForPushNotifications();
-  }
-
-  componentWillUnmount() {
-    this._notificationSubscription && this._notificationSubscription.remove();
-  }
 
   render() {
     return (
@@ -70,24 +63,7 @@ export default class RootNavigation extends React.Component {
       />
     );
   }
-
-  _registerForPushNotifications() {
-    // Send our push token over to our backend so we can receive notifications
-    // You can comment the following line out if you want to stop receiving
-    // a notification every time you open the app. Check out the source
-    // for this function in api/registerForPushNotificationsAsync.js
-    registerForPushNotificationsAsync();
-
-    // Watch for incoming notifications
-    this._notificationSubscription = Notifications.addListener(this._handleNotification);
-  }
-
-  _handleNotification = ({origin, data}) => {
-    this.props.navigator.showLocalAlert(
-      `Push notification ${origin} with data: ${JSON.stringify(data)}`,
-      Alerts.notice
-    );
-  }
+  
 }
 
 const styles = StyleSheet.create({
