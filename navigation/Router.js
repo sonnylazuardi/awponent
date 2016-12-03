d4e4ce87e7cec81413d6394b8ca319f24ed0d269
Sonny Lazuardi <sonnylazuardi@gmail.com>
2016-12-03 10:49:51 +0700

    remove unused page

--- navigation/Router.js
+++ navigation/Router.js
@@ -5,8 +5,6 @@ import {
 import HomeScreen from '../screens/HomeScreen';
 import Landing from '../screens/Landing';
 import Liked from '../screens/Liked';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
 import RootNavigation from './RootNavigation';
 
 export default createRouter(() => ({
